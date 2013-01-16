819622c9b60b14d56fc442d43c40bd37a7a452c9
Tatu Saloranta <tsaloranta@gmail.com>
2013-01-15 20:36:48 -0800

    adding more skeletal stuff

--- src/main/java/com/fasterxml/jackson/dataformat/protobuf/ProtobufSchemaLoader.java
+++ src/main/java/com/fasterxml/jackson/dataformat/protobuf/ProtobufSchemaLoader.java
@@ -1,8 +1,87 @@
 package com.fasterxml.jackson.dataformat.protobuf;
 
+import java.io.*;
+import java.net.URL;
+
+import com.dyuproject.protostuff.parser.Proto;
+import com.dyuproject.protostuff.parser.ProtoUtil;
+
 public class ProtobufSchemaLoader
 {
     protected final static ProtobufSchemaLoader DEFAULT_INSTANCE = new ProtobufSchemaLoader();
     
     public ProtobufSchemaLoader() { }
+
+    /*
+    /**********************************************************
+    /* Public API
+    /**********************************************************
+     */
+    
+    public ProtobufSchema load(URL url) throws IOException
+    {
+        Proto proto = new Proto();
+        try {
+            ProtoUtil.loadFrom(url, proto);
+        } catch (Exception e) {
+            _throw(e);
+        }
+
+        // !!! TODO
+        
+        return null;
+    }
+
+    public ProtobufSchema load(File f) throws IOException
+    {
+        Proto proto = new Proto(f);
+        try {
+            ProtoUtil.loadFrom(f, proto);
+        } catch (Exception e) {
+            _throw(e);
+        }
+
+        // !!! TODO
+        
+        return null;
+    }
+
+    public ProtobufSchema load(String schemaAsString) throws IOException
+    {
+        Proto proto = new Proto();
+        // this is crazy but...
+        InputStream in = new ByteArrayInputStream(schemaAsString.getBytes("UTF-8"));
+        try {
+            ProtoUtil.loadFrom(in, proto);
+        } catch (Exception e) {
+            _throw(e);
+        }
+        in.close();
+
+        // !!! TODO
+        
+        return null;
+    }
+
+    /*
+    /**********************************************************
+    /* Helper methods
+    /**********************************************************
+     */
+
+    public void _throw(Exception e0) throws IOException
+    {
+        // First, peel it
+        Throwable e = e0;
+        while (e.getCause() != null) {
+            e = e.getCause();
+        }
+        if (e instanceof RuntimeException) {
+            throw (RuntimeException) e;
+        }
+        if (e instanceof IOException){ 
+            throw (IOException) e;
+        }
+        throw new IOException(e.getMessage(), e);
+    }
 }
