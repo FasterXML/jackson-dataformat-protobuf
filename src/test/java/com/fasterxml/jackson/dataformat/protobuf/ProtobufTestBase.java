4c90346ad62b413c43076ff2e491957bdd162a73
Tatu Saloranta <tsaloranta@gmail.com>
2013-04-18 18:47:25 -0700

    ...

---
+++ src/test/java/com/fasterxml/jackson/dataformat/protobuf/ProtobufTestBase.java
@@ -0,0 +1,52 @@
+package com.fasterxml.jackson.dataformat.protobuf;
+import java.util.Arrays;
+
+import com.fasterxml.jackson.core.*;
+
+import junit.framework.TestCase;
+
+public class ProtobufTestBase extends TestCase
+{
+    /*
+    /**********************************************************
+    /* Additional assertion methods
+    /**********************************************************
+     */
+
+    protected void assertToken(JsonToken expToken, JsonToken actToken)
+    {
+        if (actToken != expToken) {
+            fail("Expected token "+expToken+", current token "+actToken);
+        }
+    }
+
+    protected void assertToken(JsonToken expToken, JsonParser jp)
+    {
+        assertToken(expToken, jp.getCurrentToken());
+    }
+
+    protected void assertType(Object ob, Class<?> expType)
+    {
+        if (ob == null) {
+            fail("Expected an object of type "+expType.getName()+", got null");
+        }
+        Class<?> cls = ob.getClass();
+        if (!expType.isAssignableFrom(cls)) {
+            fail("Expected type "+expType.getName()+", got "+cls.getName());
+        }
+    }
+
+    protected void verifyException(Throwable e, String... matches)
+    {
+        String msg = e.getMessage();
+        String lmsg = (msg == null) ? "" : msg.toLowerCase();
+        for (String match : matches) {
+            String lmatch = match.toLowerCase();
+            if (lmsg.indexOf(lmatch) >= 0) {
+                return;
+            }
+        }
+        fail("Expected an exception with one of substrings ("+Arrays.asList(matches)+"): got one with message \""+msg+"\"");
+    }
+
+}
