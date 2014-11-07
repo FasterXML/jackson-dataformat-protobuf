763f28d045879f9f8df6ea15de645a7d258ebc13
Cowtowncoder <tatu.saloranta@iki.fi>
2014-11-07 12:29:01 -0800

    fixes to tests now pass

--- src/main/java/com/fasterxml/jackson/dataformat/protobuf/ByteAccumulator.java
+++ src/main/java/com/fasterxml/jackson/dataformat/protobuf/ByteAccumulator.java
@@ -10,28 +10,60 @@ public class ByteAccumulator
 {
     protected final ByteAccumulator _parent;
 
+    protected final int _typedTag;
+    
     // !!! TEMP: inefficient, only used during initial prototyping
     protected final ByteArrayOutputStream _bytes = new ByteArrayOutputStream(200);
-    
-    public ByteAccumulator(ByteAccumulator p) {
+
+    public ByteAccumulator(ByteAccumulator p, int typedTag) {
         _parent = p;
+        _typedTag = typedTag;
     }
 
     public void append(byte[] buf, int offset, int len) {
         _bytes.write(buf, offset, len);
     }
 
-    public ByteAccumulator finish(OutputStream out) throws IOException
+    public ByteAccumulator finish(OutputStream out,
+            byte[] input, int offset, int len) throws IOException
     {
         byte[] payload = _bytes.toByteArray();
-        int plen = payload.length;
+        int plen = payload.length + len;
+
+        byte[] tagBytes = ProtobufUtil.lengthAsBytes(_typedTag);
         byte[] lbytes = ProtobufUtil.lengthAsBytes(plen);
         
         // root? Just output it all 
         if (_parent == null) {
+            out.write(tagBytes);
             out.write(lbytes);
-            out.write(lbytes);
+            out.write(payload);
+            if (len > 0) {
+                out.write(input, offset, len);
+            }
         } else {
+            _parent.append(tagBytes, 0, tagBytes.length);
+            _parent.append(lbytes, 0, lbytes.length);
+            _parent.append(payload, 0, payload.length);
+            _parent.append(input, offset, len);
+        }
+        return _parent;
+    }
+    
+    public ByteAccumulator finish(OutputStream out) throws IOException
+    {
+        byte[] payload = _bytes.toByteArray();
+        int plen = payload.length;
+        byte[] tagBytes = ProtobufUtil.lengthAsBytes(_typedTag);
+        byte[] lbytes = ProtobufUtil.lengthAsBytes(plen);
+        
+        // root? Just output it all 
+        if (_parent == null) {
+            out.write(tagBytes);
+            out.write(lbytes);
+            out.write(payload);
+        } else {
+            _parent.append(tagBytes, 0, tagBytes.length);
             _parent.append(lbytes, 0, lbytes.length);
             _parent.append(payload, 0, payload.length);
         }
