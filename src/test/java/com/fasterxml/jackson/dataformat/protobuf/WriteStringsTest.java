bf0369d3a8d0efe4c4ac78490e1a2b1f54711ba9
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-11-11 20:08:53 -0800

    ...

--- src/test/java/com/fasterxml/jackson/dataformat/protobuf/WriteStringsTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/protobuf/WriteStringsTest.java
@@ -63,7 +63,7 @@ public class WriteStringsTest extends ProtobufTestBase
         _testSimpleLong(90, "\u2009\u3333");
         _testSimpleLong(129, "\u2009\u3333");
         _testSimpleLong(2007, "abc\u3333");
-        _testSimpleLong(5000, "");
+        _testSimpleLong(5000, "\u2009b\u3333a");
     }
     
     private void _testSimpleLong(int clen, String part) throws Exception
